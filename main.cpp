468a8b0f1e3d40825e16a3d463336a530e7641d2
OonisiHiroki <k021g1328@g.neec.ac.jp>
2022-10-19 14:51:51 +0900

    サウンド再生(途中)

--- main.cpp
+++ main.cpp
@@ -1,20 +1,187 @@
 ﻿#include "WinApp.h"
 #include "DirectXCommon.h"
 #include "GameScene.h"
+#include<wrl.h>
+
+#include <fstream>
+#include <xaudio2.h>
+#pragma comment (lib,"xaudio2.lib")
+
+using namespace Microsoft::WRL;
+
+//チャンクヘッダ
+struct ChunkHeader {
+	char id[4];		//チャンク毎のID
+	int32_t size;	//チャンクサイズ
+};
+
+//RIFFヘッダチャンク
+struct RiffHeader {
+	ChunkHeader chunk;	//"RIFF"
+	char type[4];		//"WAVE"
+};
+
+//FMTチャンク
+struct FormatChunk {
+	ChunkHeader chunk;	//"fmt"
+	WAVEFORMATEX fmt;	//波形フォーマット
+};
+
+//音声データ
+struct SoundData {
+	//波形フォーマット
+	WAVEFORMATEX wfex;
+	//バッファの先頭アドレス
+	BYTE* pBuffer;
+	//バッファサイズ
+	unsigned int bufferSize;
+};
+
+//音声データの読み込み
+SoundData SoundLoadWave(const char* filename) {
+
+	//HRESULT result;
+
+	//-------①ファイルオープン-------//
+
+	//ファイル入力ストリームのインスタンス
+	std::ifstream file;
+	//.wavファイルをバイナリモードで開く
+	file.open(filename, std::ios_base::binary);
+	//ファイルオープン失敗を検出する
+	assert(file.is_open());
+
+	//-------②.wavデータ読み込み-------//
+
+	//RIFFヘッダーの読み込み
+	RiffHeader riff;
+	file.read((char*)&riff, sizeof(riff));
+
+	//ファイルがRIFFかチェック
+	if (strncmp(riff.chunk.id, "RIFF", 4) != 0) {
+		assert(0);
+	}
+
+	//タイプがWAVEかチェック
+	if (strncmp(riff.type, "WAVE", 4) != 0) {
+		assert(0);
+	}
+
+	//Formatチャンクの読み込み
+	FormatChunk format = {};
+
+	//チャンクヘッダーの確認
+	file.read((char*)&format, sizeof(ChunkHeader));
+	if (strncmp(format.chunk.id, "fmt ", 4) != 0) {
+		assert(0);
+	}
+
+	//チャンク本体の読み込み
+	assert(format.chunk.size <= sizeof(format.fmt));
+	file.read((char*)&format.fmt, format.chunk.size);
+
+	//Dataチャンクの読み込み
+	ChunkHeader data;
+	file.read((char*)&data, sizeof(data));
+
+	//JUNKチャンクを検出した場合
+	if (strncmp(data.id, "JUNK ", 4) == 0) {
+		//読み込み位置をJUNKチャンクの終わるまで進める
+		file.seekg(data.size, std::ios_base::cur);
+		//再読み込み
+		file.read((char*)&data, sizeof(data));
+	}
+
+	if (strncmp(data.id, "data ", 4) != 0) {
+		assert(0);
+	}
+
+	//Dataチャンクのデータ部(波形データ)の読み込み
+	char* pBuffer = new char[data.size];
+	file.read(pBuffer, data.size);
+
+	//Waveファイルを閉じる
+	file.close();
+
+	//-------③読み込んだ音声データをreturn-------//
+
+	//returnする為の音声データ
+	SoundData soundData = {};
+
+	soundData.wfex = format.fmt;
+	soundData.pBuffer = reinterpret_cast<BYTE*>(pBuffer);
+	soundData.bufferSize = data.size;
+
+	return soundData;
+
+}
+
+//-------音声データの解放-------//
+void SoundUnload(SoundData* soundData) {
+	//バッファのメモリを解放
+	delete[] soundData->pBuffer;
+
+	soundData->pBuffer = 0;
+	soundData->bufferSize = 0;
+	soundData->wfex = {};
+}
+
+//------サウンドの再生-------//
+
+//音声再生
+void SoundPlayWave(IXAudio2* xAudio2, const SoundData& soundData) {
+
+	HRESULT result;
+
+	//波形フォーマットを元にSourceVoiceの生成
+	IXAudio2SourceVoice* pSourceVoice = nullptr;
+	result = xAudio2->CreateSourceVoice(&pSourceVoice, &soundData.wfex);
+	assert(SUCCEEDED(result));
+
+	//再生する波形データの設定
+	XAUDIO2_BUFFER buf{};
+	buf.pAudioData = soundData.pBuffer;
+	buf.AudioBytes = soundData.bufferSize;
+	buf.Flags = XAUDIO2_END_OF_STREAM;
+
+	//波形データの再生
+	result = pSourceVoice->SubmitSourceBuffer(&buf);
+	result = pSourceVoice->Start();
+
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
-{
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	HRESULT result;
+
+	//サウンド再生
+	ComPtr<IXAudio2> xAudio2 ;
+	IXAudio2MasteringVoice* masterVoice;
+
+	//XAudioエンジンのインスタンスを生成
+	result = XAudio2Create(&xAudio2, 0, XAUDIO2_DEFAULT_PROCESSOR);
+	//マスターボイスを生成
+	result = xAudio2->CreateMasteringVoice(&masterVoice);
+
+
+	
+
+	//音声読み込み
+	SoundData soundData1 = SoundLoadWave("Resources/Alarm01.wav");
+	//音声再生
+	SoundPlayWave(xAudio2.Get(), soundData1);
+
 	// 汎用機能
 	WinApp* win = nullptr;
 	DirectXCommon* dxCommon = nullptr;
-	Input* input = nullptr;	
+	Input* input = nullptr;
 	GameScene* gameScene = nullptr;
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
 	win->CreateGameWindow();
-		
+
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
 	dxCommon->Initialize(win);
@@ -26,7 +193,7 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
 
 	// スプライト静的初期化
 	Sprite::StaticInitialize(dxCommon->GetDevice(), WinApp::kWindowWidth, WinApp::kWindowHeight);
-	
+
 	// 3Dオブジェクト静的初期化
 	Object3d::StaticInitialize(dxCommon->GetDevice(), WinApp::kWindowWidth, WinApp::kWindowHeight);
 #pragma endregion
@@ -34,18 +201,19 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
 	// ゲームシーンの初期化
 	gameScene = new GameScene();
 	gameScene->Initialize(dxCommon, input);
+
 	
+	
+
 	// メインループ
-	while (true)
-	{
+	while (true) {
 		// メッセージ処理
-		if (win->ProcessMessage()) {	break; }
+		if (win->ProcessMessage()) { break; }
 
 		// 入力関連の毎フレーム処理
 		input->Update();
 		// ゲームシーンの毎フレーム処理
 		gameScene->Update();
-
 		// 描画開始
 		dxCommon->PreDraw();
 		// ゲームシーンの描画
@@ -61,6 +229,11 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
 	dxCommon->Finalize();
 	// ゲームウィンドウの破棄
 	win->TerminateGameWindow();
+
+	//XAudio2解放
+	xAudio2.Reset();
+	//音声データ解放
+	SoundUnload(&soundData1);
 
 	return 0;
 }
